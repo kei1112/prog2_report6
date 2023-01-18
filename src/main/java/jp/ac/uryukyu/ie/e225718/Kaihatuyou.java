738f8f5494a9722ab9d321dd199e73603a9b4f0b
Kuroshima Kei <e225718@ie.u-ryukyu.ac.jp>
2023-01-18 16:40:00 +0900

    3rd cmmit

--- src/main/java/jp/ac/uryukyu/ie/e225718/Kaihatuyou.java
+++ src/main/java/jp/ac/uryukyu/ie/e225718/Kaihatuyou.java
@@ -1,6 +1,6 @@
 package jp.ac.uryukyu.ie.e225718;
 
 public abstract class Kaihatuyou {
-    public abstract void honnbann(int number);
-    
+    public abstract void kakunin(int number);//確認する
+    public abstract void ooki();//大きいものを出す
 }
