27a445a4f016542eeae67f3559fb135d165c9e8a
Kuroshima Kei <e225718@ie.u-ryukyu.ac.jp>
2023-01-20 23:54:38 +0900

    8th commit

--- src/main/java/jp/ac/uryukyu/ie/e225718/Game.java
+++ src/main/java/jp/ac/uryukyu/ie/e225718/Game.java
@@ -4,7 +4,13 @@ import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
 
-//ごかくし
+/**
+ * ごかくしゲームの実装
+ * 
+ * 
+ * 
+ * 
+ */
 
 class Sabu{
     String name;
@@ -17,33 +23,47 @@ class Sabu{
 
 public class Game extends Sabu{
 
+    /**
+     * ユーザーの名前を登録するメソッド
+     */
     public void setName(String username){
         this.name = username;
     }
-    public void setNumber(int number){
-        this.number = number;
-    }
+
+    /**
+     * ユーザーの名前をゲットするメソッド
+     */
     public String getName(){
         return this.name;
     }
-    public int getNumber(){
-        return this.number;
-    }
+
     //コンストラクタ
     Game(String username){
         this.setName(username);
         System.out.println("・ユーザーネームを" + name + "で始めるね♡\n  私の名前はみちぇだよ");   
     }
-    //メソッド1 説明する
+
+    /**
+     * ゲームの説明をする
+     */
     public void explain(){
         System.out.println("＊今から説明するよ＊\n1 まず、1~5の中で1つ数字を選ぶ\n2 みちぇが、君の選んだ数より大きい数を言うよ\n3 君はそれより大きい数を選ぼう\n4 相手が5を選んだ時はパスしなければならないよ\n5 5つ数を言えたら勝ちだよ");
     }
 
     //メソッド2 マスターの中身
+    /**
+     * ユーザーが数字を5つ選択し、勝利するまで続ける <br>
+     * ユーザーが5を選択した場合は、パスをし、数字は0を選択することにする <br>
+     * ゲームのマスターの選ぶ数字は、ランダムに選択ようにする <br>
+     * まだ選んでない数字　かつ　ユーザーの選んだ数字より大きい場合　ー＞　その数字を選択
+     * 
+     * @param number ユーザーの選んだ数字
+     */
     public void master(int number){//number>>相手が選んだ数
         //ユーザーより大きい数を選ぶ。ルールに則って
         int i = 1;
         while(i == 1 && numbers.size()<5){
+
             if(number == 5){
                 System.out.println("----- みちぇは パス をした -----");
                 game(0);
@@ -68,14 +88,26 @@ public class Game extends Sabu{
         System.out.println("~~ ゲーム終了 " + name + " の勝ち ~~");
         System.exit(0);
     }
-    //メソッド3-2 ゲームの内容  >>>　勝ち負け決める
+
+
+    /**
+     * マスターが数字を5つ選択し、勝利するまで続ける <br>
+     * 
+     * 数字を打ち込んでもらう <br>
+     * マスターが5を選択した場合は、パスし、0を選択することにする <br>
+     * まだ選んでない数字　かつ　マスターの選んだ数字より大きい場合　ー＞　その数字を選択 <br>
+     * 
+     * @param number ゲームマスターの選んだ数字
+     */
     public void game(int number){//number = master の数字
         int input;
         int i = 1;
 
         Scanner in = new Scanner(System.in);
         System.out.println("~~~~~ " + name + "のターンだよ ~~~~~");
+
         while(i == 1 && numbers2.size() < 5){
+
             if(number == 5 ){
                 System.out.println("~~ パスしか出せないよ(・Д・) ~~");
                 master(0);
@@ -85,6 +117,7 @@ public class Game extends Sabu{
                 input = in.nextInt();
                 boolean result = numbers.contains(input);
                 boolean okisa = input > number;
+
                 if(!result && okisa){
                     System.out.println("~~ " + name + " は " + input + " を選んだ ~~");
                     numbers.add(input);
@@ -94,7 +127,8 @@ public class Game extends Sabu{
                     i = 1;
                 }
             }
-        }in.close();
+        }
+        in.close();
         System.out.println("~~ ゲーム終了 みちぇの勝ち ~~");
         System.exit(0);
         
